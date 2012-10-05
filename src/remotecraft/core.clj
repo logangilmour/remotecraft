a7d2402c838954c19b086329b666f40efb736c3f
Logan Gilmour <logangilmour@uws16968usr.uws.ualberta.ca>
2012-10-05 13:52:32 -0600

    add server skeleton

--- src/remotecraft/core.clj
+++ src/remotecraft/core.clj
@@ -1,1 +1,18 @@
-(ns remotecraft.core)
+(ns remotecraft.core
+  (:use compojure.core
+        [clojure.tools.logging :only (info error)])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            ))
+
+(defroutes main-routes
+
+  (GET "/init" []
+       (do (logger/init config/database) "Successfully initialized views."))
+
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def app
+  (handler/site main-routes))
+
